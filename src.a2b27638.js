9f82f8c12570abdc9a68e76c6c7276721e4132d3
pralphv <chowzhiho321@gmail.com>
2022-09-11 16:42:06 -0400

    Updates

--- src.a2b27638.js
+++ src.a2b27638.js
@@ -117,79 +117,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
-
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
-  }
-
-  return bundleURL;
-}
-
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
-
-    if (matches) {
-      return getBaseURL(matches[0]);
-    }
-  }
-
-  return '/';
-}
-
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)?\/[^/]+(?:\?.*)?$/, '$1') + '/';
-}
-
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
-  };
-
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
-}
-
-var cssTimeout = null;
-
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
-  }
-
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
-    }
-
-    cssTimeout = null;
-  }, 50);
-}
-
-module.exports = reloadCSS;
-},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"src/styles.css":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/promise-polyfill/src/finally.js":[function(require,module,exports) {
+})({"node_modules/promise-polyfill/src/finally.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -52646,8 +52574,6 @@ exports.default = _default;
 },{"pixi.js":"node_modules/pixi.js/dist/esm/pixi.mjs","../../assets/sprites/enemy/enemy.png":"assets/sprites/enemy/enemy.png","../../assets/sprites/enemy/data":"assets/sprites/enemy/data.js","./model":"src/classes/model.js"}],"src/index.js":[function(require,module,exports) {
 "use strict";
 
-require("./styles.css");
-
 var PIXI = _interopRequireWildcard(require("pixi.js"));
 
 var _player = _interopRequireDefault(require("./classes/player"));
@@ -52933,7 +52859,7 @@ function _main() {
 }
 
 main();
-},{"./styles.css":"src/styles.css","pixi.js":"node_modules/pixi.js/dist/esm/pixi.mjs","./classes/player":"src/classes/player.js","./classes/Enemy":"src/classes/Enemy.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"pixi.js":"node_modules/pixi.js/dist/esm/pixi.mjs","./classes/player":"src/classes/player.js","./classes/Enemy":"src/classes/Enemy.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
